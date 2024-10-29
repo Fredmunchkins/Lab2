25f6132da4adbbcbd9cc4db24c7de75fcbb9dbde
Dylan <dy555tay@gmail.com>
2024-10-29 10:47:24 +0800

    updated lab2

--- bmi.py
+++ bmi.py
@@ -9,10 +9,13 @@ def calculate_bmi(height, weight):
     # Determine weight classification
     if bmi < 18.5:
         print("Classification: Under Weight")
+        return bmi,-1
     elif 18.5 <= bmi <= 25.0:
         print("Classification: Normal Weight")
+        return bmi,0
     else:
         print("Classification: Over Weight")
+        return bmi,1
 
 # Call the function with height and weight
 calculate_bmi(weight=57, height=1.73)
